55c5f027035ff23e5b1f7e279c299e72f8a74713
Adir Kuhn <adirkuhn@gmail.com>
2015-09-04 19:38:31 +0000

    Create new types of LVAL

--- type.c
+++ type.c
@@ -1,19 +1,40 @@
 #include "type.h"
 
 /* create a new number of type lval */
-lval lval_num(long x) {
-	lval v;
-	v.type = LVAL_NUM;
-	v.num = x;
+lval* lval_num(long x) {
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_NUM;
+	v->num = x;
 
 	return v;
 }
 
 /* create a new error of type lval */
-lval lval_err(int x) {
-	lval v;
-	v.type = LVAL_ERR;
-	v.err = x;
+lval* lval_err(char* e) {
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_ERR;
+	v->err = malloc(strlen(e) + 1);
+	strcpy(v->err, m);
+
+	return v;
+}
+
+/* create a new symbol of type lval */
+lval* lval_sym(char* s) {
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_SYM;
+	v->sym = malloc(strlen(s) + 1);
+	strcpy(v->sym, s);
+
+	return v;
+}
+
+/* create a new Sexpr of type lval */
+lval* lval_sexpr(void) {
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_SEXPR;
+	v->count = 0;
+	v->cell = NULL;
 
 	return v;
 }
